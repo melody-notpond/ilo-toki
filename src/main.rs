6eb12db47c11188eaea41ec9c7e6d59e9f4d32f7
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2022-11-10 12:49:15 -0500

    Added nicer text editing capabilities

--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,20 @@ struct Message {
     content: String,
 }
 
+enum Mode {
+    Insert,
+    Normal,
+}
+
 struct AppState {
     messages: Vec<Message>,
+
     input_text: String,
+    input_char_pos: usize,
+    input_byte_pos: usize,
+
     client: Arc<Client>,
+    mode: Mode,
 }
 
 static RUNNING: AtomicBool = AtomicBool::new(true);
@@ -56,12 +66,16 @@ async fn main() -> Result<(), io::Error> {
     let state = AppState {
         messages: vec![],
         input_text: String::new(),
+        input_char_pos: 0,
+        input_byte_pos: 0,
         client: client.clone(),
+        mode: Mode::Insert,
     };
     let state = Arc::new(Mutex::new(state));
     {
-        let state2 = state.clone();
         let lock = state.lock().await;
+
+        let state2 = state.clone();
         lock.client
             .add_event_handler(move |event: SyncRoomMessageEvent| {
                 let state = state2.clone();
@@ -79,7 +93,6 @@ async fn main() -> Result<(), io::Error> {
                     }
                 }
             });
-
     }
 
     client.sync_once(SyncSettings::default()).await.unwrap();
@@ -95,6 +108,7 @@ async fn main_ui(state: Arc<Mutex<AppState>>) -> Result<(), io::Error> {
     crossterm::execute!(io::stdout(), EnterAlternateScreen)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+    let mut stdout = io::stdout();
     crossterm::terminal::enable_raw_mode()?;
     terminal.clear()?;
 
@@ -124,8 +138,54 @@ async fn main_ui(state: Arc<Mutex<AppState>>) -> Result<(), io::Error> {
             let input = widgets::Paragraph::new(state.input_text.as_str()).block(input);
             f.render_widget(input, vertical[1]);
 
-            let status = widgets::Paragraph::new("uwu");
+            let status = {
+                match state.mode {
+                    Mode::Insert => Span::raw("INSERT"),
+                    Mode::Normal => Span::raw("NORMAL"),
+                }
+            };
+            let status = widgets::Paragraph::new(status);
             f.render_widget(status, vertical[2]);
+
+            match state.mode {
+                Mode::Insert => {
+                    use crossterm::cursor::{CursorShape, SetCursorShape};
+                    crossterm::execute!(stdout, SetCursorShape(CursorShape::Line)).unwrap();
+                    let m = state.input_char_pos as u16 % (vertical[1].width - 2);
+                    if m == 0 && state.input_char_pos != 0 {
+                        f.set_cursor(
+                            vertical[1].x + vertical[1].width - 1,
+                            vertical[1].y
+                                + (state.input_char_pos as u16 - 1) / (vertical[1].width - 2)
+                                + 1,
+                        );
+                    } else {
+                        f.set_cursor(
+                            vertical[1].x + m + 1,
+                            vertical[1].y + state.input_char_pos as u16 / (vertical[1].width - 2) + 1,
+                        );
+                    }
+                }
+
+                Mode::Normal => {
+                    use crossterm::cursor::{CursorShape, SetCursorShape};
+                    crossterm::execute!(stdout, SetCursorShape(CursorShape::Block)).unwrap();
+                    let m = state.input_char_pos as u16 % (vertical[1].width - 2);
+                    if m == 0 && state.input_char_pos != 0 {
+                        f.set_cursor(
+                            vertical[1].x + vertical[1].width - 1,
+                            vertical[1].y
+                                + (state.input_char_pos as u16 - 1) / (vertical[1].width - 2)
+                                + 1,
+                        );
+                    } else {
+                        f.set_cursor(
+                            vertical[1].x + m + 1,
+                            vertical[1].y + state.input_char_pos as u16 / (vertical[1].width - 2) + 1,
+                        );
+                    }
+                }
+            }
         })?;
 
         tokio::time::sleep(Duration::from_millis(10)).await;
@@ -143,63 +203,200 @@ async fn ui_events(state: Arc<Mutex<AppState>>) {
     let room = state.lock().await.client.joined_rooms().swap_remove(0);
 
     while let Ok(Ok(event)) = tokio::task::spawn_blocking(crossterm::event::read).await {
-        match event {
-            Event::FocusGained => (),
-            Event::FocusLost => (),
-            Event::Resize(_, _) => (),
+        let mut state = state.lock().await;
+        match state.mode {
+            Mode::Insert => {
+                match event {
+                    Event::FocusGained => (),
+                    Event::FocusLost => (),
+                    Event::Resize(_, _) => (),
 
-            Event::Key(key) => match key.code {
-                KeyCode::Backspace => (),
+                    Event::Key(key) => match key.code {
+                        KeyCode::Backspace => {
+                            if state.input_byte_pos > 0 {
+                                let mut i = 1;
+                                while !state.input_text.is_char_boundary(state.input_byte_pos - i) {
+                                    i += 1;
+                                }
+                                state.input_byte_pos -= i;
+                                state.input_char_pos -= 1;
+                                let pos = state.input_byte_pos;
+                                state.input_text.remove(pos);
+                            }
+                        }
 
-                KeyCode::Enter => {
-                    let mut state = state.lock().await;
-                    room.send(
-                        RoomMessageEventContent::text_plain(state.input_text.clone()),
-                        None,
-                    )
-                    .await
-                    .unwrap();
-                    state.input_text.clear();
+                        KeyCode::Enter => {
+                            if state.input_text == "/quit" {
+                                RUNNING.store(false, Ordering::Release);
+                                break;
+                            }
+                            if !state.input_text.is_empty() {
+                                room.send(
+                                    RoomMessageEventContent::text_plain(state.input_text.clone()),
+                                    None,
+                                )
+                                .await
+                                .unwrap();
+                                state.input_text.clear();
+                                state.input_char_pos = 0;
+                                state.input_byte_pos = 0;
+                            }
+                        }
+
+                        KeyCode::Up => (),
+                        KeyCode::Down => (),
+                        KeyCode::Home => (),
+                        KeyCode::End => (),
+                        KeyCode::PageUp => (),
+                        KeyCode::PageDown => (),
+                        KeyCode::Tab => (),
+                        KeyCode::BackTab => (),
+                        KeyCode::Delete => (),
+                        KeyCode::Insert => (),
+                        KeyCode::F(_) => (),
+
+
+                        KeyCode::Left => {
+                            if state.input_byte_pos > 0 {
+                                let mut i = 1;
+                                while !state.input_text.is_char_boundary(state.input_byte_pos - i) {
+                                    i += 1;
+                                }
+                                state.input_byte_pos -= i;
+                                state.input_char_pos -= 1;
+                            }
+                        }
+
+                        KeyCode::Right => {
+                            if state.input_byte_pos < state.input_text.bytes().len() {
+                                let mut i = 1;
+                                while !state.input_text.is_char_boundary(state.input_byte_pos + i) {
+                                    i += 1;
+                                }
+                                state.input_byte_pos += i;
+                                state.input_char_pos += 1;
+                            }
+                        }
+
+
+                        KeyCode::Char(c) => {
+                            let pos = state.input_byte_pos;
+                            state.input_text.insert(pos, c);
+                            state.input_byte_pos += c.len_utf8();
+                            state.input_char_pos += 1;
+                        }
+
+                        KeyCode::Null => (),
+
+                        KeyCode::Esc => {
+                            state.mode = Mode::Normal;
+                        }
+
+                        KeyCode::CapsLock => (),
+                        KeyCode::ScrollLock => (),
+                        KeyCode::NumLock => (),
+                        KeyCode::PrintScreen => (),
+                        KeyCode::Pause => (),
+                        KeyCode::Menu => (),
+                        KeyCode::KeypadBegin => (),
+                        KeyCode::Media(_) => (),
+                        KeyCode::Modifier(_) => (),
+                    }
+
+                    Event::Mouse(_) => (),
+                    Event::Paste(_) => (),
+                }               
+            }
+
+            Mode::Normal => {
+                match event {
+                    Event::FocusGained => (),
+                    Event::FocusLost => (),
+
+                    Event::Key(key) => {
+                        match key.code {
+                            KeyCode::Backspace => (),
+                            KeyCode::Enter => {
+                                if state.input_text == "/quit" {
+                                    RUNNING.store(false, Ordering::Release);
+                                    break;
+                                }
+                                if !state.input_text.is_empty() {
+                                    if state.input_text == "/quit" {
+                                        RUNNING.store(false, Ordering::Release);
+                                        break;
+                                    }
+                                    room.send(
+                                        RoomMessageEventContent::text_plain(state.input_text.clone()),
+                                        None,
+                                    )
+                                    .await
+                                    .unwrap();
+                                    state.input_text.clear();
+                                    state.input_char_pos = 0;
+                                    state.input_byte_pos = 0;
+                                }
+                            }
+                            KeyCode::Up => (),
+                            KeyCode::Down => (),
+                            KeyCode::Home => (),
+                            KeyCode::End => (),
+                            KeyCode::PageUp => (),
+                            KeyCode::PageDown => (),
+                            KeyCode::Tab => (),
+                            KeyCode::BackTab => (),
+                            KeyCode::Delete => (),
+                            KeyCode::Insert => (),
+                            KeyCode::F(_) => (),
+
+                            KeyCode::Char('i') => {
+                                state.mode = Mode::Insert;
+                            }
+
+                            KeyCode::Char('h') | KeyCode::Left => {
+                                if state.input_byte_pos > 0 {
+                                    let mut i = 1;
+                                    while !state.input_text.is_char_boundary(state.input_byte_pos - i) {
+                                        i += 1;
+                                    }
+                                    state.input_byte_pos -= i;
+                                    state.input_char_pos -= 1;
+                                }
+                            }
+
+                            KeyCode::Char('l') | KeyCode::Right => {
+                                if state.input_byte_pos < state.input_text.bytes().len() {
+                                    let mut i = 1;
+                                    while !state.input_text.is_char_boundary(state.input_byte_pos + i) {
+                                        i += 1;
+                                    }
+                                    state.input_byte_pos += i;
+                                    state.input_char_pos += 1;
+                                }
+                            }
+
+                            KeyCode::Char(_) => (),
+
+                            KeyCode::Null => (),
+                            KeyCode::Esc => (),
+                            KeyCode::CapsLock => (),
+                            KeyCode::ScrollLock => (),
+                            KeyCode::NumLock => (),
+                            KeyCode::PrintScreen => (),
+                            KeyCode::Pause => (),
+                            KeyCode::Menu => (),
+                            KeyCode::KeypadBegin => (),
+                            KeyCode::Media(_) => (),
+                            KeyCode::Modifier(_) => (),
+                        }
+                    }
+
+                    Event::Mouse(_) => (),
+                    Event::Paste(_) => (),
+                    Event::Resize(_, _) => (),
                 }
-
-                KeyCode::Left => (),
-                KeyCode::Right => (),
-                KeyCode::Up => (),
-                KeyCode::Down => (),
-                KeyCode::Home => (),
-                KeyCode::End => (),
-                KeyCode::PageUp => (),
-                KeyCode::PageDown => (),
-                KeyCode::Tab => (),
-                KeyCode::BackTab => (),
-                KeyCode::Delete => (),
-                KeyCode::Insert => (),
-                KeyCode::F(_) => (),
-
-                KeyCode::Char(c) => {
-                    state.lock().await.input_text.push(c);
-                }
-
-                KeyCode::Null => (),
-
-                KeyCode::Esc => {
-                    RUNNING.store(false, Ordering::Release);
-                    break;
-                }
-
-                KeyCode::CapsLock => (),
-                KeyCode::ScrollLock => (),
-                KeyCode::NumLock => (),
-                KeyCode::PrintScreen => (),
-                KeyCode::Pause => (),
-                KeyCode::Menu => (),
-                KeyCode::KeypadBegin => (),
-                KeyCode::Media(_) => (),
-                KeyCode::Modifier(_) => (),
-            },
-
-            Event::Mouse(_) => (),
-            Event::Paste(_) => (),
+            }
         }
+
     }
 }
